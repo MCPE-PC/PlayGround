7c90553f75ecd2fa459aac5025b3253c114ede7d
MCPE_PC <maxpjh0528@naver.com>
2018-03-11 17:07:47 +0900

    Update LinuxKiller.sh

--- LinuxKiller.sh
+++ LinuxKiller.sh
@@ -3,9 +3,6 @@ KILL_LEVEL=0
 
 getopts "0123" OPT
 case $OPT in
-	0)
-		$KILL_LEVEL=0
-		;;
 	1)
 		$KILL_LEVEL=1
 		;;
@@ -22,6 +19,6 @@ esac
 
 echo "Linux Killer script running with kill level $KILL_LEVEL..."
 
-read -p "This will remove your computer's data. Are you sure? [y/N] " -e CONFIRM
+read -e -p "This will remove your computer's data. Are you sure? [y/N] " CONFIRM
 if [ echo "$CONFIRM" | tr [:upper:] [:lower:] -ne "y"] then echo "Hello, World!"; exit; fi
 echo "ne"
