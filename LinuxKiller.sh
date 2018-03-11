789b548bba8034f58b63f059ef8e8f87b178ad4d
MCPE_PC <maxpjh0528@naver.com>
2018-03-11 17:03:46 +0900

    Test

---
+++ LinuxKiller.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+KILL_LEVEL=0
+
+getopts "0123" OPT
+case $OPT in
+	0)
+		$KILL_LEVEL=0
+		;;
+	1)
+		$KILL_LEVEL=1
+		;;
+	2)
+		$KILL_LEVEL=2
+		;;
+	3)
+		$KILL_LEVEL=3
+		;;
+	\?)
+		exit 1
+		;;
+esac
+
+echo "Linux Killer script running with kill level $KILL_LEVEL..."
+
+read -p "This will remove your computer's data. Are you sure? [y/N] " -e CONFIRM
+if [ echo "$CONFIRM" | tr [:upper:] [:lower:] -ne "y"] then echo "Hello, World!"; exit; done
+echo "ne"
