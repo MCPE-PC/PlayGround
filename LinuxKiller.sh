f521f03df41a3255511e4fc7954d2f7a5635bda6
MCPE_PC <maxpjh0528@naver.com>
2018-03-11 17:12:26 +0900

    Update LinuxKiller.sh

--- LinuxKiller.sh
+++ LinuxKiller.sh
@@ -17,5 +17,8 @@ esac
 echo "Linux Killer script running with kill level $KILL_LEVEL..."
 
 read -p "This will remove your computer's data. Are you sure? [y/N] " CONFIRM
-if [ echo "$CONFIRM" | tr [:upper:] [:lower:] -ne "y"] then echo "n"; exit; fi
+if [ echo "$CONFIRM" | tr [:upper:] [:lower:] -ne "y"] then
+	echo "n"
+	exit
+fi
 echo "y"
